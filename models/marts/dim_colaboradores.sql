6ebf6102613392a229252a10f28d33e92897c023
Gabriel Nogueira <gabriel2.nogueira@alunos.ifsuldeminas.edu.br>
2025-01-25 04:02:45 +0000

    Atualizando a dim_colaboradores

--- models/marts/dim_colaboradores.sql
+++ models/marts/dim_colaboradores.sql
@@ -2,16 +2,31 @@
 
 with stg_colaboradores as (
     select * from {{ ref('stg_colaboradores') }}
+),
+stg_colaboradores_agencia as (
+    select * from {{ ref('stg_colaborador_agencia')}}
+),
+
+joined_table as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['stg_colaboradores.colaborador_id', 'stg_colaboradores.colaborador_email', 'stg_colaboradores_agencia.agencia_id']) }} as colaborador_sk,
+        stg_colaboradores.colaborador_id as colaborador_id,
+        stg_colaboradores_agencia.agencia_id as agencia_id,
+        stg_colaboradores.colaborador_nome as colaborador_nome,
+        stg_colaboradores.colaborador_sobrenome as colaborador_sobrenome,
+        stg_colaboradores.colaborador_email as colaborador_email
+    from stg_colaboradores_agencia
+    left join stg_colaboradores
+        on stg_colaboradores_agencia.colaborador_id = stg_colaboradores.colaborador_id    
+
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['colaborador_id', 'colaborador_email', 'data_nascimento']) }} as colaborador_sk,
+    colaborador_sk,
     colaborador_id,
+    agencia_id,
     colaborador_nome,
     colaborador_sobrenome,
-    colaborador_email,
-    colaborador_cpf,
-    data_nascimento,
-    colaborador_endereco,
-    colaborador_cep
-from stg_colaboradores
+    colaborador_email
+from joined_table
+order by colaborador_id
--- models/marts/fact_financeiro.sql
+++ models/marts/fact_financeiro.sql
@@ -13,22 +13,29 @@ with
     dim_propostas_credito as (
         select * from {{ ref('dim_propostas_credito') }}
     ),
-    dim_transacoes as (
-        select * from {{ ref('dim_transacoes') }}
-    ),
     dim_dates as (
-        select *from {{ref('dim_dates')}}
+        select * from {{ ref('dim_dates') }}
     ),
     fact_table as (
-        select * from {{ref('int_bv_conta')}}
+        select * from {{ ref('int_bv_conta') }}
     ),
 
+    dim_transacoes_ranked as (
+        select
+            *,
+            row_number() over (
+                partition by conta_id 
+                order by data_transacao desc
+            ) as row_num
+        from {{ ref('dim_transacoes') }}
+    ),
 
     joined_table as (
         select
             {{ dbt_utils.generate_surrogate_key(['fact_table.cliente_id', 'dim_agencias.agencia_sk', 'dim_clientes.cliente_sk']) }} as financeiro_sk,
             fact_table.cliente_id as cliente_id,
             fact_table.conta_id as conta_id,
+            dim_dates.metric_date as data_fk,
             dim_agencias.agencia_sk as agencia_fk,
             dim_clientes.cliente_sk as cliente_fk,
             dim_colaboradores.colaborador_sk as colaborador_fk,
@@ -37,21 +44,27 @@ with
             dim_transacoes.data_transacao,
             fact_table.saldo_total,
             fact_table.saldo_disponivel,
+            fact_table.data_abertura,
+            fact_table.data_ultimo_lancamento,
             dim_transacoes.valor_transacao
         from fact_table
         left join dim_agencias
             on fact_table.agencia_id = dim_agencias.agencia_id
         left join dim_clientes
             on fact_table.cliente_id = dim_clientes.cliente_id
+            and fact_table.conta_id = dim_clientes.cliente_id
         left join dim_colaboradores
             on fact_table.colaborador_id = dim_colaboradores.colaborador_id
+            and fact_table.agencia_id = dim_colaboradores.agencia_id
         left join dim_dates
             on fact_table.data_abertura = dim_dates.metric_date
-        left join dim_transacoes
+        left join dim_transacoes_ranked dim_transacoes
             on fact_table.conta_id = dim_transacoes.conta_id
+            and dim_transacoes.data_transacao between fact_table.data_abertura and fact_table.data_ultimo_lancamento
+            and dim_transacoes.row_num = 1
         left join dim_propostas_credito
-            on fact_table.cliente_id = dim_propostas_credito.cliente_id
-            and fact_table.colaborador_id = dim_propostas_credito.colaborador_id
+            on fact_table.colaborador_id = dim_propostas_credito.colaborador_id
+            and fact_table.cliente_id = dim_propostas_credito.cliente_id
     )
 
 select
@@ -63,7 +76,10 @@ select
     colaborador_fk,
     proposta_fk,
     transacao_fk,
+    data_fk,
     data_transacao,
+    data_abertura,
+    data_ultimo_lancamento,
     saldo_total,
     saldo_disponivel,
     valor_transacao
